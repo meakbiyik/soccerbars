ccba211000a0feca759694d9b4d4cd69e25bb2e3
M. Eren Akbiyik <erenakbiyik@gmail.com>
2020-12-04 00:46:35 +0300

    Formatting

--- R/core.R
+++ R/core.R
@@ -25,12 +25,12 @@ goal_to_height <- function(x) {
 #' @return ggplot objects of the plots created.
 #' @examples
 #' plot_scores(list(
-#'      list(1,2,T), list(3,3,F), list(0,2,T), list(0,0,F), list(6,6,T)
+#'     list(1, 2, T), list(3, 3, F), list(0, 2, T), list(0, 0, F), list(6, 6, T)
 #' ))
 #' plot_scores(list(
-#'      list(1,2,T), list(3,3,F), list(0,2,T), list(0,0,F), list(6,6,T),
-#'      list(0,2,F), list(0,0,F), list(6,6,T), list(6,3,T)
-#' ), outlined=T, output_path = "out.png")
+#'     list(1, 2, T), list(3, 3, F), list(0, 2, T), list(0, 0, F),
+#'     list(6, 6, T), list(0, 2, F), list(0, 0, F), list(6, 6, T), list(6, 3, T)
+#' ), outlined = T, output_path = "out.png")
 #' @export
 plot_scores <- function(scores,
                         twogoalline = FALSE,
@@ -38,7 +38,6 @@ plot_scores <- function(scores,
                         outlined = FALSE,
                         show = TRUE,
                         output_path = NULL) {
-
     scores <- maybe_convert_dataframe(scores)
 
     check_scores(scores)
@@ -57,10 +56,14 @@ plot_scores <- function(scores,
 
         if (twogoalline) {
             two_goals <- goal_to_height(2)
-            lines <- append(lines, list(c(0, two_goals,
-                                          match_count, two_goals)))
-            lines <- append(lines, list(c(0, -two_goals,
-                                          match_count, -two_goals)))
+            lines <- append(lines, list(c(
+                0, two_goals,
+                match_count, two_goals
+            )))
+            lines <- append(lines, list(c(
+                0, -two_goals,
+                match_count, -two_goals
+            )))
         }
 
         for (index in seq_along(matches)) {
@@ -76,8 +79,10 @@ plot_scores <- function(scores,
                 offset1 <- match_index - slope * goal_to_height(scores[[2]])
                 height0 <- goal_to_height(scores[[1]])
                 height1 <- -goal_to_height(scores[[2]])
-                lines <- append(lines, list(c(offset1, height1,
-                                              offset0, height0)))
+                lines <- append(lines, list(c(
+                    offset1, height1,
+                    offset0, height0
+                )))
             } else {
                 lines <- append(lines, circle(match_index, 0))
             }
@@ -132,12 +137,18 @@ check_scores <- function(scores) {
         first_match <- scores[[1]]
     }
     checkmate::assert_list(first_match, min.len = 1, .var.name = "match vector")
-    checkmate::assert_int(first_match[[1]], null.ok = FALSE,
-                          .var.name = "Scores of the home team")
-    checkmate::assert_int(first_match[[2]], null.ok = FALSE,
-                          .var.name = "Scores of the away team")
-    checkmate::assert_logical(first_match[[3]], len = 1, null.ok = FALSE,
-                              any.missing = FALSE, .var.name = "Away flag")
+    checkmate::assert_int(first_match[[1]],
+        null.ok = FALSE,
+        .var.name = "Scores of the home team"
+    )
+    checkmate::assert_int(first_match[[2]],
+        null.ok = FALSE,
+        .var.name = "Scores of the away team"
+    )
+    checkmate::assert_logical(first_match[[3]],
+        len = 1, null.ok = FALSE,
+        any.missing = FALSE, .var.name = "Away flag"
+    )
 }
 
 dot <- function(x, y) circle(x, y, radius = 0.1)
@@ -164,31 +175,42 @@ plot <- function(lines,
     h_line_count <- if (twogoalline) 3 else 1
 
     lines_dataframe <- as.data.frame(do.call(rbind, lines))
-    linewidths <- c(rep_len(c(0.2), h_line_count),
-                    rep_len(c(5), length(lines) - h_line_count))
+    linewidths <- c(
+        rep_len(c(0.2), h_line_count),
+        rep_len(c(5), length(lines) - h_line_count)
+    )
 
     ax <- ggplot(lines_dataframe, dpi = 300) +
         aes(x = V1, y = V2, xend = V3, yend = V4) +
         coord_fixed() +
         geom_segment(size = linewidths, lineend = "round") +
-        xlim(-0.5, match_count + 0.5) + ylim(-3.2, 3.2) +
+        xlim(-0.5, match_count + 0.5) +
+        ylim(-3.2, 3.2) +
         theme_void() +
         labs(x = NULL, y = NULL)
 
     if (outlined) {
-        outline_linewidths <- c(rep_len(c(0.2), h_line_count),
-                              rep_len(c(2.5), length(lines) - h_line_count))
-        outline_colors <- c(rep_len(c("black"), h_line_count),
-                          rep_len(c("white"), length(lines) - h_line_count))
-        ax <- ax + geom_segment(size = outline_linewidths,
-                                         lineend = "round",
-                                         color = outline_colors)
+        outline_linewidths <- c(
+            rep_len(c(0.2), h_line_count),
+            rep_len(c(2.5), length(lines) - h_line_count)
+        )
+        outline_colors <- c(
+            rep_len(c("black"), h_line_count),
+            rep_len(c("white"), length(lines) - h_line_count)
+        )
+        ax <- ax + geom_segment(
+            size = outline_linewidths,
+            lineend = "round",
+            color = outline_colors
+        )
     }
 
     if (!is.null(output_path)) {
-        ggsave(output_path, plot = ax,
-                        height = 4, width = 0.5 * match_count,
-                        type = "cairo")
+        ggsave(output_path,
+            plot = ax,
+            height = 4, width = 0.5 * match_count,
+            type = "cairo"
+        )
     }
     if (show) {
         print(ax)
